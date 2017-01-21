1f335471bc3b423ea47238d49dba00f193041bb1
Sava Minić <minic.sava@gmail.com>
2017-01-21 11:24:26 +0100

    Updated test colors

--- MexicanWave/Assets/Scripts/SingleSeat.cs
+++ MexicanWave/Assets/Scripts/SingleSeat.cs
@@ -7,7 +7,6 @@ public class SingleSeat : MonoBehaviour
 	public int x { get; private set; }
 	public int y { get; private set; }
 	public StandsView view { get; private set; }
-	public Color color { get; private set; }
 
 	private Renderer myRenderer;
 
@@ -21,13 +20,12 @@ public class SingleSeat : MonoBehaviour
 		this.x = x;
 		this.y = y;
 		this.view = view;
-		this.color = new Color(Random.value, Random.value, Random.value);
 	}
 
 	void Update()
 	{
 		var val = StandsController.I.Value(x, y);
-		myRenderer.material.color = new Color(color.r, color.g, color.b, Mathf.Clamp(val, 0.1f, 1f));
+		myRenderer.material.color = new Color(val, 0.5f, 0.5f, 0.5f);
 	}
 
 }
